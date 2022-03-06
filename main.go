df76e94245fc271670d3787cf7dd74c113eddcb3
Edison Li <edison4354@gmail.com>
2022-03-06 05:27:12 -0800

    Handling input arguments and printing the link

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	. "github.com/logrusorgru/aurora"
 )
 
 func main() {
-	fmt.Print(Bold("Hello"))
+	link := ""
+	if len(os.Args) == 2 {
+		link = string(os.Args[1])
+	} else {
+		log.Fatal("\nError while running - Usage: go run main.go <link>\n Please make sure you entered the link correctly")
+	}
+	fmt.Println(Bold(link))
 }
